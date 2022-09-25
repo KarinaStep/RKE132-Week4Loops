8518fd4c4f08a3562e148d91c19fec217b7a961e
Karina <karinastepp@gmail.com>
2022-09-25 21:40:46 +0300

    add week2loops code

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true; //bool võib olla true/false
+
+while(loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine($"cpu has generated {cpuRandom}."); //sellega saab näha ebaausalt, mis numbri programm genereeris
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+
+    int userNumber = Int32.Parse(Console.ReadLine()); //algul loeb programm kasutaja vastust sõne formaadis ja siis muudab numbriks
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        //loopActive = false //vaja selleks, et kui kasutaja vastus on õige, siis väljutakse loopist ja soovitakse head päeva
+        break; // võib kasutada "loopActive = false" asemel
+    }
+    else 
+    {
+        Console.WriteLine("Oops. Try again.");
+    }
+}
+
+Console.WriteLine("Have a nice day!");
